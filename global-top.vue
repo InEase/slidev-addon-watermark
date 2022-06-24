13d125c5c33c0fd9422bf155a843853f6bc1d301
InEase <InEase28@gmail.com>
2022-06-25 00:55:12 +0800

    feat: finish proposal

--- global-top.vue
+++ global-top.vue
@@ -1,2 +1,10 @@
 <template>
+  <input type="number" v-model="width" style="top: 0px;">
+  <Watermark :width="width" />
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const width = ref(180);
+</script>
